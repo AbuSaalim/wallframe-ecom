74b4636c396cb329d2efac521123a98f740eb573
AbuSaalim <salim.whitestone@gmail.com>
2026-02-01 09:00:48 +0530

    UI: Add ToastContainer to GlobalProvider so showToast displays

--- components/Application/GlobalProvider.jsx
+++ components/Application/GlobalProvider.jsx
@@ -7,6 +7,8 @@ import { PersistGate } from 'redux-persist/integration/react'
 import Loading from './Loading'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 const queryClient = new QueryClient()
 
@@ -16,6 +18,7 @@ const GlobalProvider = ({children}) => {
     <Provider store={store}>
         <PersistGate persistor={persistor} loading={<Loading/>} >
         {children}
+        <ToastContainer />
         </PersistGate>
 
     </Provider>
